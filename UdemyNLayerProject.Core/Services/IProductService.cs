9eefe941b37b781a177f23eaa7f1dc4a47607865
savasevtr <savas.ev@yahoo.com>
2021-01-02 14:50:58 +0300

    asp.net core web/api best practices

---
+++ UdemyNLayerProject.Core/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Models;
+
+namespace UdemyNLayerProject.Core.Services
+{
+    public interface IProductService : IService<Product>
+    {
+        Task<Product> GetWithCategoryByIdAsync(int id);
+        // bool ControlInnerBarcode(Product product);
+    }
+}

d6166cf9b8320524c562b91c55389ef14bb1cb2b
savasevtr <savas.ev@yahoo.com>
2021-01-03 21:43:50 +0300

    Person Table Added

--- UdemyNLayerProject.Data/Context/AppDbContext.cs
+++ UdemyNLayerProject.Data/Context/AppDbContext.cs
@@ -14,13 +14,16 @@ namespace UdemyNLayerProject.Data.Context
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Person> Persons { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new PersonConfiguration());
+            // modelBuilder.Entity<Person>().Property(x => x.Name).HasMaxLength(100);
 
-            modelBuilder.ApplyConfiguration(new CategorySeed(new int[] { 1,2 }));
+            modelBuilder.ApplyConfiguration(new CategorySeed(new int[] { 1, 2 }));
             modelBuilder.ApplyConfiguration(new ProductSeed(new int[] { 1, 2 }));
         }
     }
